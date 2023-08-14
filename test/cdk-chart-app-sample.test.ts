9e4f7be203a3dc1badf0af5a7fd00b56f55a9e2d
Kenji Kono <konoken@amazon.co.jp>
2023-08-14 09:38:52 +0900

    add snapshot test

--- test/cdk-chart-app-sample.test.ts
+++ test/cdk-chart-app-sample.test.ts
@@ -1,16 +1,45 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkChartAppSample from '../lib/cdk-chart-app-sample-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { PipelineStack } from '../lib/pipeline-stack';
+import { BackendStack } from '../lib/backend-stack';
+import { FrontendStack } from '../lib/frontend-stack';
+import { APIStack } from '../lib/api-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-chart-app-sample-stack.ts
-test('SQS Queue Created', () => {
-  //   const app = new cdk.App();
-  //     // WHEN
-  //   const stack = new CdkChartAppSample.CdkChartAppSampleStack(app, 'MyTestStack');
-  //     // THEN
-  //   const template = Template.fromStack(stack);
-  //   template.hasResourceProperties('AWS::SQS::Queue', {
-  //     VisibilityTimeout: 300
-  //   });
+test('PipelineStack matches snapshot', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new PipelineStack(app, 'Pipeline', {
+    prefix: 'Summit',
+    repo: 'aws-samples/cdk-chart-app-sample',
+    branch: 'main',
+    connectionArn:
+      'arn:aws:codestar-connections:ap-northeast-1:123456789012:connection/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+  });
+  // THEN
+  const template = Template.fromStack(stack);
+  expect(template.toJSON()).toMatchSnapshot();
+});
+
+describe('Stacks match snapshots', () => {
+  const app = new cdk.App();
+  // WHEN
+  const backend = new BackendStack(app, `Backend`);
+
+  const api = new APIStack(app, `API`, {
+    currentTable: backend.currentTable,
+  });
+
+  const frontend = new FrontendStack(app, `Frontend`);
+
+  test('BackendStack matches snapshot', () => {
+    expect(Template.fromStack(backend).toJSON()).toMatchSnapshot();
+  });
+
+  test('APIStack matches snapshot', () => {
+    expect(Template.fromStack(api).toJSON()).toMatchSnapshot();
+  });
+
+  test('FrontendStack matches snapshot', () => {
+    expect(Template.fromStack(frontend).toJSON()).toMatchSnapshot();
+  });
 });
